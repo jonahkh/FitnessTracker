c82f9736ba6606520d0a78b4f9d0f39be6d7c33e
jonah <jonahkh@uw.edu>
2016-04-26 21:40:49 -0700

    add more stuff

--- app/src/main/java/jonahkh/tacoma/uw/edu/fitnesstracker/FacebookFragment.java
+++ app/src/main/java/jonahkh/tacoma/uw/edu/fitnesstracker/FacebookFragment.java
@@ -3,12 +3,15 @@ package jonahkh.tacoma.uw.edu.fitnesstracker;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -32,35 +35,36 @@ public class FacebookFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_facebook, container, false);
         LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email");
-        // If using in a fragment
-        loginButton.setFragment(this);
         loginButton = (LoginButton) view.findViewById(R.id.login_button);
         List<String> permissions = new ArrayList<String>();
         permissions.add("email");
         permissions.add("public_profile");
         loginButton.setReadPermissions(permissions);
-        // If using in a fragment
         loginButton.setFragment(this);
+
+        // If using in a fragment
+
         // Other app specific specialization
 
 //        // Callback registration
-//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                // App code
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//                // App code
-//            }
-//
-//            @Override
-//            public void onError(FacebookException exception) {
-//                // App code
-//            }
-//        });
+        CallbackManager callback = CallbackManager.Factory.create();
+        Log.e("TAG", "here");
+        loginButton.registerCallback(callback, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.e("TAG", "Success");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("TAG", "Cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e("TAG", "ERROR");
+            }
+        });
         return inflater.inflate(R.layout.fragment_facebook, container, false);
 
     }
