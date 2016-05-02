15f956103013553e60d3b50d6e9fdd93c2264ddf
Jonahk <jonahkh@uw.edu>
2016-05-02 14:33:14 -0700

    Newest version

--- app/src/main/java/jonahkh/tacoma/uw/edu/fitnesstracker/dashboard/DashboardActivity.java
+++ app/src/main/java/jonahkh/tacoma/uw/edu/fitnesstracker/dashboard/DashboardActivity.java
@@ -6,10 +6,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
+//import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,11 +19,6 @@ import android.view.MenuItem;
 
 import jonahkh.tacoma.uw.edu.fitnesstracker.R;
 import jonahkh.tacoma.uw.edu.fitnesstracker.authentication.LoginActivity;
-import jonahkh.tacoma.uw.edu.fitnesstracker.dashboard.MyExerciseExpandableListAdapter;
-import jonahkh.tacoma.uw.edu.fitnesstracker.dashboard.PreDefinedWorkout;
-import jonahkh.tacoma.uw.edu.fitnesstracker.dashboard.PreDefinedWorkoutFragment;
-import jonahkh.tacoma.uw.edu.fitnesstracker.dashboard.WeightWorkout;
-import jonahkh.tacoma.uw.edu.fitnesstracker.dashboard.WeightWorkoutListFragment;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -51,6 +47,8 @@ public class DashboardActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        // TODO see if support lib
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
